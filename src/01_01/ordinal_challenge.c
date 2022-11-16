2e03bedae4efbe27c5c0577537c52b006dde0f25
jtomar <jtomar@users.noreply.github.com>
2022-11-16 17:42:45 +0000

    wrote body of ordinal function

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -3,9 +3,27 @@
 char *ordinal(int v)
 {
 	/*
-	   - create this function
-	   - this code won't run with out it!
+		 - create this function
+		 - this code won't run with out it!
 	 */
+	char *ch[4] = {"st", "nd", "rd", "th"};
+
+	if (v % 10 == 1)
+	{
+		return ch[0];
+	}
+	else if (v % 10 == 2)
+	{
+		return ch[1];
+	}
+	else if (v % 10 == 3)
+	{
+		return ch[2];
+	}
+	else
+	{
+		return ch[3];
+	}
 }
 
 int main()
@@ -13,16 +31,15 @@ int main()
 	int c;
 
 	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	return (0);
 }
